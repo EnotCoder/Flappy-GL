e5a4e7e1760298784fbcc545c7e0d17cc02a3ca7
EnotCoder <samaks.sasha@gmail.com>
2026-02-14 15:06:02 +0300

    fix code main

--- main.cpp
+++ main.cpp
@@ -31,46 +31,39 @@ unsigned int frag[] = {
     1,2,3
 };
 
-
+//Random number
 double getRandomDouble(double min, double max) {
     static std::mt19937 gen{std::random_device{}()};
     std::uniform_real_distribution<double> dis(min, max);
     return dis(gen);
 }
 
-void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
-    double xpos, ypos;
-    glfwGetCursorPos(window, &xpos, &ypos);
-
-    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
-
-    }
-
-}
-
-
+//MAIN
 int main()
-{
+{   
+    //Glfw INIT
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);    
 
+
+    //Window
     GLFWwindow* window = glfwCreateWindow(1200, 800, "C++ game", NULL, NULL);
     if (window == NULL){std::cout << "ERROR WINDOW CREATE";glfwTerminate();}
 
     glfwSetWindowPos(window,500,500);
     glfwMakeContextCurrent(window);    
 
+    //GLAD INIT
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {   
         std::cout << "ERROR GLAD INIT";
         return -1;
     }
 
-    //stbi_set_flip_vertically_on_load(true);
-
+    //VAO VBO EBO
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
@@ -89,69 +82,57 @@ int main()
     glGenBuffers(1,&EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(frag), frag, GL_STATIC_DRAW);
-    
 
-    shaderProgram = CreateShader("./shaders/Vert.glsl", "./shaders/Frag.glsl");
-    glUseProgram(shaderProgram);
 
+    //glEnable
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    //shaders
+    shaderProgram = CreateShader("./shaders/Vert.glsl", "./shaders/Frag.glsl");
 
     SetupTextBuffers();
     unsigned int textShader = CreateShader("./shaders/textVert.glsl", "./shaders/textFrag.glsl");
     InitText(20);
 
-
     initUniforms();
 
+    //fon sprite
     Sprite FonTexture;
     FonTexture.Texture = CreateTexture("tex/Background/Background2.png");
     FonTexture.rectSize = glm::vec2(2.0,1.0);
     FonTexture.scale = glm::vec2(4.0,2.0);
     unsigned int starTexture = CreateTexture("tex/star.png");
 
-    glfwSetMouseButtonCallback(window, mouse_button_callback);
-
     //player
-
     player bird;
 
-    //star
-
-    anim star;
-    star.Texture = CreateTexture("tex/star.png");
-    star.currentUv = glm::vec2(1.0,1.0);
-    star.framesCount = 5;
-    star.frameTime = 0.15;
-    star.cutX = 5;
-    star.cutY = 1;
-
     //tube
-
     Sprite tube;
     tube.Texture = CreateTexture("tex/Tiles/PipeStyle1.png");
     tube.cutX = 4;tube.cutY = 1;tube.isCollide = true;
     tube.scale = glm::vec2(0.3,1.5);tube.uvFrame = glm::vec2(1.0,1.0);
 
+    //down tubes
     Sprite tube1 = tube;tube1.position = glm::vec2(2.0,-(static_cast <float>(getRandomDouble(1.0,1.35))));
     Sprite tube2 = tube;tube2.position = glm::vec2(3.5,-(static_cast <float>(getRandomDouble(1.0,1.35))));
     Sprite tube3 = tube;tube3.position = glm::vec2(5.0,-(static_cast <float>(getRandomDouble(1.0,1.35))));
-
     Sprite tubesDown[] = {tube1, tube2, tube3};
 
+    //up tubes
     tube.angle = 180;
     Sprite tube4 = tube;tube4.position = glm::vec2(2.0,(static_cast <float>(getRandomDouble(1.0,1.35))));
     Sprite tube5 = tube;tube5.position = glm::vec2(3.5,(static_cast <float>(getRandomDouble(1.0,1.35))));
     Sprite tube6 = tube;tube6.position = glm::vec2(5.0,(static_cast <float>(getRandomDouble(1.0,1.35))));
-
     Sprite tubesUp[] = {tube4, tube5, tube6};
 
+    //global var
     int store = 0;
 
     //while
     float lastTime = glfwGetTime();
     while (!glfwWindowShouldClose(window))
-    {   
+    {
         //delta time
         float now = glfwGetTime();
         float deltaTime = now - lastTime;
@@ -163,6 +144,7 @@ int main()
             glfwSetWindowShouldClose(window, true);
         }
 
+        //restart
         if (glfwGetKey(window,GLFW_KEY_R) == GLFW_PRESS)
         {
             tubesDown[0].position = glm::vec2(2.0,-(static_cast <float>(getRandomDouble(1.0,1.35))));
@@ -183,11 +165,13 @@ int main()
         //Draw Object
         glClearColor(30.0f/255, 28.0f/255, 52.0f/255, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        areaCollide.clear();
+        glUseProgram(shaderProgram);
 
         //fon
         FonTexture.draw();
 
+
+        //tubes
         areaCollide.clear();
         
         //tube
@@ -209,22 +193,15 @@ int main()
         //player
         bird.move(window,deltaTime);
 
-        //stars
-        //star.play(deltaTime, glm::vec2(0.2), glm::vec2(0.0), 0, false);
-
+        //glm::mat4 projection = glm::ortho(0.0f, 1200.0f, 800.0f, 0.0f);
+        //glUniformMatrix4fv(glGetUniformLocation(textShader, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+        //text
         glUseProgram(textShader);
-        glm::mat4 projection = glm::ortho(0.0f, 1200.0f, 800.0f, 0.0f);
-        glUniformMatrix4fv(glGetUniformLocation(textShader, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         RenderText(textShader, "Store: " + std::to_string(store), 50.0f, 700.0f, 1.0f, glm::vec3(1.0f, 1.0f, 1.0f));
-
-        if (!bird.active)
-        {
-            RenderText(textShader,"Presed R-restart", 750.0f, 700.0f, 1.0f, glm::vec3(1.0f, 1.0f, 1.0f));
-        }
-
-        glUseProgram(shaderProgram);
+        if (!bird.active)RenderText(textShader,"Presed R-restart", 750.0f, 700.0f, 1.0f, glm::vec3(1.0f, 1.0f, 1.0f));
 
 
+        //window
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
